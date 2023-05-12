ec560ae8fb88dfd4c7e33617b110ca1491b8ed54
juan.cardona <felipe7u7432@gmail.com>
2023-05-11 22:56:02 -0500

    Creacion proyecto base

---
+++ QuickjobApi/src/test/java/co/edu/uco/quickjob/api/quickjobapi/QuickjobApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.quickjob.api.quickjobapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickjobApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
