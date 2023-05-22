ea8ddc0613d2479b6767747d86ce97ab12e6e603
juan.cardona <felipe7u7432@gmail.com>
2023-05-21 22:26:30 -0500

    Completando DTOs y configuracion de Application

--- QuickjobApi/src/main/java/co/edu/uco/quickjob/api/init/QuickjobApiApplication.java
+++ QuickjobApi/src/main/java/co/edu/uco/quickjob/api/init/QuickjobApiApplication.java
@@ -2,11 +2,16 @@ package co.edu.uco.quickjob.api.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@ComponentScan(basePackages ="co.edu.uco.quickjob" )
 public class QuickjobApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(QuickjobApiApplication.class, args);
     }
 
--- QuickjobDTO/src/main/java/co/edu/uco/quickjob/dto/UserDTO.java
+++ QuickjobDTO/src/main/java/co/edu/uco/quickjob/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package co.edu.uco.quickjob.dto;
 
-import java.util.UUID;
+import co.edu.uco.crosscutting.util.UtilObject;
+
+import static co.edu.uco.crosscutting.util.UtilText.*;
+import static co.edu.uco.crosscutting.util.UtilText.trim;
 
 public class UserDTO {
     private String id;
@@ -8,4 +11,91 @@ public class UserDTO {
     private String email;
     private String password;
     private String image;
+
+    private PersonDTO person;
+
+    public UserDTO(String id, String name, String email, String password, String image, PersonDTO person) {
+        setId(id);
+        setName(name);
+        setEmail(email);
+        setPassword(password);
+        setPerson(person);
+        setImage(image);
+    }
+
+    public UserDTO() {
+        setId(EMPTY);
+        setName(EMPTY);
+        setEmail(EMPTY);
+        setPassword(EMPTY);
+        setPerson(PersonDTO.create());
+        setImage(EMPTY);
+    }
+
+    public PersonDTO getPerson() {
+        if (UtilObject.getUtilObject().isNull(person)){
+            setPerson(person);
+        }
+        return person;
+    }
+
+    public void setPerson(PersonDTO person) {
+        this.person = UtilObject.getUtilObject().getDefaultIsNull(person, PersonDTO.create());
+    }
+
+    public String getId() {
+        if (isNull(id)){
+            setId(EMPTY);
+        }
+        return trim(id);
+    }
+
+    public void setId(String id) {
+        this.id = trim(id);
+    }
+
+    public String getName() {
+        if (isNull(name)){
+            setId(EMPTY);
+        }
+        return trim(name);
+    }
+
+    public void setName(String name) {
+        this.name = trim(name);
+    }
+
+
+    public String getEmail() {
+        if(isNull(email)){
+            setEmail(email);
+        }
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = trim(email);
+    }
+
+    public String getPassword() {
+        if (isNull(password)){
+            setPassword(password);
+        }
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = trim(password);
+    }
+
+    public String getImage() {
+        if(isNull(image)){
+            setImage(image);
+        }
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
